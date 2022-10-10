948504895fce2b4273b526c19618e34e565af15b
MScott1607 <ayamasa.sg@protonmail.com>
2022-10-10 17:50:31 +0100

    Extract requests logic to helpers.py

--- app.py
+++ app.py
@@ -1,17 +1,8 @@
 from flask import Flask
-import requests
+from helpers import get_pokemon_by_name
 
 
 app = Flask(__name__)
-
-
-pokemon_creatures = {
-    "bulbasaur": "dinosaur",
-    "charmander": "reptile",
-    "pikachu": "rodent",
-    "eevee": "fox",
-    "diglett": "mole",
-}
 
 
 @app.get("/")
@@ -21,13 +12,12 @@ def pokemon_list():
 
 @app.get("/<pokemon_name>")
 def pokemon_data(pokemon_name):
-    response = requests.get(f"https://pokeapi.co/api/v2/pokemon/{pokemon_name}")
-    pokemon = response.json()
-    return f"This is {pokemon['name'].title()}. \n" \
-           f"Height: {pokemon['height']}. \n" \
-           f"Weight: {pokemon['weight']}. \n" \
-           f"Base experience: {pokemon['base_experience']}. \n" \
-           f"Type: {' and '.join(type_info['type']['name'] for type_info in pokemon['types'])} \n"
+    pokemon = get_pokemon_by_name(pokemon_name)
+    return f"This is {pokemon['name']}.\n" \
+           f"Height: {pokemon['height']}.\n" \
+           f"Weight: {pokemon['weight']}.\n" \
+           f"Base experience: {pokemon['base_experience']}.\n" \
+           f"Type(s): {' and '.join(type_info['type']['name'] for type_info in pokemon['types'])}"
 
 
 if __name__ == "__main__":
